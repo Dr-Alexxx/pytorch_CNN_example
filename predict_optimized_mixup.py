18a09cf6ec6550845665c90a42023826f19bc597
Yi_Dream <dongym2007@163.com>
2026-01-02 13:08:39 +0800

    修复上一个版本预测脚本定义模型bug

--- predict_optimized_mixup.py
+++ predict_optimized_mixup.py
@@ -37,17 +37,17 @@ class ResidualBlock(nn.Module):
 class OptimizedCNN(nn.Module):
     def __init__(self, num_classes=10):
         super(OptimizedCNN, self).__init__()
-        # 注意：这里必须与训练时的"加宽"配置保持一致 (64, 128, 256)
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
         self.relu = nn.ReLU(inplace=True)
 
-        self.layer1 = self._make_layer(64, 64, 3, stride=1)
-        self.layer2 = self._make_layer(64, 128, 3, stride=2) 
-        self.layer3 = self._make_layer(128, 256, 3, stride=2)
+        self.layer1 = self._make_layer(32, 32, 2, stride=1)
+        self.layer2 = self._make_layer(32, 64, 2, stride=2) 
+        self.layer3 = self._make_layer(64, 128, 2, stride=2)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(256, num_classes)
+        self.fc = nn.Linear(128, num_classes)
 
     def _make_layer(self, in_channels, out_channels, num_blocks, stride):
         layers = [ResidualBlock(in_channels, out_channels, stride)]
